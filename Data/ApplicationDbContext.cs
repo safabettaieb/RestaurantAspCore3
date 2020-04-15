c63fd0d61938bdc4dbd745e62b289c52c0a09452
Safa Bettaieb <safa.bettaieb1@gmail.com>
2020-04-15 20:56:54 +0100

    Create Category Model

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using RestaurantAspCore3.Models;
 
 namespace RestaurantAspCore3.Data
 {
@@ -12,5 +13,6 @@ namespace RestaurantAspCore3.Data
             : base(options)
         {
         }
+        public DbSet<Category> categories { get; set; }
     }
 }
---
+++ Data/Migrations/20200415194154_AddCategory.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RestaurantAspCore3.Data.Migrations
+{
+    public partial class AddCategory : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "categories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_categories", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "categories");
+        }
+    }
+}
